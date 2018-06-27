a6df5ab4b499854d384fa5a55fa3a8ff7b7d6c3f
Venu Daroju <darojuv@mail.com>
2018-06-27 03:01:10 -0700

    redux store dispatch

--- redux-basics.js
+++ redux-basics.js
@@ -1,10 +1,34 @@
 const redux = require('redux');
 
 const initialState = {counter:0};
-
+//Reducer
 const rootReducer = function(state = initialState, action){
+    if(action.type === 'INC_COUNTER'){
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    };
+    if(action.type === 'ADD_COUNTER'){
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    };
     return state;
 }
-
+//Store
 const store = redux.createStore(rootReducer );
 console.log(store.getState());
+// Dispatcher
+store.dispatch({
+    type:"INC_COUNTER"
+});
+console.log(store.getState());
+store.dispatch({
+    type:"ADD_COUNTER",
+    value: 10
+});
+console.log(store.getState());
+
+//Subscription
