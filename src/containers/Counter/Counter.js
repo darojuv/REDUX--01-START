c14072f444f6ce2e6e0b762248cf9d3c7fe4542f
Venu Daroju <darojuv@mail.com>
2018-06-30 02:07:39 -0700

    with actionType out sourced as constant

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import * as actionTypes from '../../store/actions';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -13,12 +14,12 @@ class Counter extends Component {
                 <CounterControl label="Increment" clicked={ this.props.onIncrementCounter } />
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={() => this.props.onAddCounter(5)}  />
-                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractResults(5)}  />
+                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter(5)}  />
                 <hr/>
                 <button onClick={this.props.onStoreResults} >Add Result</button>
             <ul>
                 {this.props.rstls.map(rslt =>
-                    <li onClick={this.props.onDeleteCounter} key={rslt.id} >{rslt.value}</li>
+                    <li onClick={() => this.props.onDeleteResults(rslt.id)} key={rslt.id} >{rslt.value}</li>
                 )}
             </ul>
             </div>
@@ -34,12 +35,12 @@ const mapStoreToProps = (reduxStore) => {
 };
 const mapDispatchToProps = dispatch => {
     return{
-        onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
-        onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
-        onAddCounter: (number) => dispatch({ type: 'ADD', value: number }),
-        onSubtractCounter: (number) => dispatch({ type: 'SUBTRACT', value: number }),      
-        onStoreResults: () => dispatch({type:'STORE_RESULTS'}),
-        onDeleteResults: () => dispatch({type:'REMOVE_RESULTS'})                  
+        onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+        onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+        onAddCounter: (number) => dispatch({ type: actionTypes.ADD, value: number }),
+        onSubtractCounter: (number) => dispatch({ type: actionTypes.SUBTRACT, value: number }),      
+        onStoreResults: () => dispatch({type:actionTypes.STORE_RESULTS}),
+        onDeleteResults: (key) => dispatch({type:actionTypes.REMOVE_RESULTS, id:key})                  
     };
 }
 export default connect(mapStoreToProps, mapDispatchToProps)(Counter);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,37 +1,46 @@
+import * as actionTypes from './actions';
+
 const initialStore = {
     counter:0,
     results:[]
 };
 
 const reducer = (state = initialStore, action) =>{
-    if(action.type === 'INCREMENT'){
+    if(action.type === actionTypes.INCREMENT){
         return{
             ...state,
             counter: state.counter + 1
         }
     }
-    if(action.type === 'DECREMENT'){
+    if(action.type === actionTypes.DECREMENT){
         return{
             ...state,
             counter: state.counter - 1
         }
     }
-    if(action.type === 'ADD'){
+    if(action.type === actionTypes.ADD){
         return{
             ...state,
             counter: state.counter + action.value
         }
     }
-    if(action.type === 'STORE_RESULTS'){
+    if(action.type === actionTypes.SUBTRACT){
+        return{
+            ...state,
+            counter: state.counter - action.value
+        }
+    }
+    if(action.type === actionTypes.STORE_RESULTS){
         return{
             ...state,
             results: state.results.concat({id:new Date(), value:state.counter })
         }
     }
-    if(action.type === 'REMOVE_RESULTS'){
+    if(action.type === actionTypes.REMOVE_RESULTS){
+        const updatedArr = state.results.filter(res => res.id !== action.id);
         return{
             ...state,
-            results: state.counter - action.value
+            results: updatedArr
         }
     }
     return state;
